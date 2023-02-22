cbeddaa5891c5cb2c98f332155c63489f09f318d
BenkoBranislav111177 <xbenkob@stuba.sk>
2023-02-22 13:33:45 +0100

    pocitanie oop

--- src/main/java/sk/stuba/fei/oop/cvicenie1/Main.java
+++ src/main/java/sk/stuba/fei/oop/cvicenie1/Main.java
@@ -1,7 +1,26 @@
 package sk.stuba.fei.oop.cvicenie1;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ahoj svjete");
+        System.out.println("Hello World!!!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Zadaj čísla: ");
+        int cisloJedna = Integer.parseInt(scanner.nextLine());
+        int cisloDva = Integer.parseInt(scanner.nextLine());
+
+        System.out.println(pocitanie(cisloJedna, cisloDva));
+
+    }
+    public static int pocitanie(int cisloJedna, int cisloDva){
+        if (cisloJedna > cisloDva){
+            return 1;
+        }
+        if (cisloJedna < cisloDva){
+            return -1;
+        }
+        return 0;
     }
 }
